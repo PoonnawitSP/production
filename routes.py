60a31fea75d7ef1dddfdc14188ddab121c4295da
poonnawit sp <poonnawit.su@utexas.edu>
2023-08-03 19:08:30 -0500

    delete

--- routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask,  request, jsonify, render_template
-from app import app
-from models import user
-
-@app.route('/signIn', methods=['POST'])
-def signIn():
-    return user().signIn()
